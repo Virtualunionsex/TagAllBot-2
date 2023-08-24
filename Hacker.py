ba0d23653a03809bd49fa1aa80d66278c0ecdfa7
MalikGit <97935254+Virtualunionsex@users.noreply.github.com>
2023-08-24 12:26:35 +0700

    Update Hacker.py

--- Hacker.py
+++ Hacker.py
@@ -12,9 +12,9 @@ logging.basicConfig(
 )
 LOGGER = logging.getLogger(__name__)
 
-api_id = int(os.environ.get("APP_ID", "16246834"))
-api_hash = os.environ.get("API_HASH", "29b3ffa9245c07f05375b92f38e8f13d")
-bot_token = os.environ.get("TOKEN", "6278243144:AAFugrKWisjrTXTrbnX3e7c4W9wVxjASptY")
+api_id = int(os.environ.get("APP_ID", "22825629"))
+api_hash = os.environ.get("API_HASH", "e8db542482a1638b4e5b03ed1ddae521")
+bot_token = os.environ.get("TOKEN", "6637498343:AAGMoAT_eAz3n19MyvyYrX0IuUUiVBTeWSo")
 client = TelegramClient('client', api_id, api_hash).start(bot_token=bot_token)
 spam_chats = []
 
